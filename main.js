fa0c8f99f72d27e10731232f5d5ac2c0f065f198
Yuchen Zhou <y353zhou@uwaterloo.ca>
2019-03-15 16:02:12 -0400

    update npm

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function createWindow () {
   // and load the index.html of the app.
   mainWindow.loadURL('http://localhost:3000')
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
